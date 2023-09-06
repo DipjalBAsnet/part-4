debd851849ae3572bfb5235534dee98813fc8476
unknown <dipjal5678@gmai.com>
2023-09-06 13:11:23 +0545

    [4.9] exercise, varified unique id

--- models/Blog.js
+++ models/Blog.js
@@ -8,8 +8,8 @@ const blogSchema = new mongoose.Schema({
 });
 
 blogSchema.set("toJSON", {
-  trasform: (document, returnedObject) => {
-    returnedObject.id = returnedObject.id.toString();
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
   },
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -10,6 +10,15 @@ test("blogs are returned as json", async () => {
     .expect(200)
     .expect("Content-Type", /application\/json/);
 }, 10000);
+
+test('blog has "_id" property', async () => {
+  const response = await api.get("/api/blogs");
+  console.log(response.body);
+  const firstBlog = response.body[0];
+
+  expect(firstBlog.id).toBeDefined();
+}, 10000);
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
