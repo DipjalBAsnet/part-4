6ff3d475269ce59fb426f41a6c77e4593807d0a0
unknown <dipjal5678@gmai.com>
2023-09-06 12:40:14 +0545

    [4.8] exercise, tested blogs returned

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,18 +2,17 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/Blog");
 const logger = require("../utils/logger");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    logger.info("saved!");
-    response.status(201).json(result);
-  });
+  const result = blog.save();
+  logger.info("saved!");
+  response.status(201).json(result);
 });
+
 module.exports = blogsRouter;
