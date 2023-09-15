9af62d43a686cc70460032d0641d1ad6607d9289
unknown <dipjal5678@gmai.com>
2023-09-15 13:41:42 +0545

    [4.15] exercise created users route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 mongoose.set("strictQuery", false);
 
@@ -22,5 +23,6 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
