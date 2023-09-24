cf745296a64f3630218dd1f756554f7b2081cc03
unknown <dipjal5678@gmai.com>
2023-09-24 14:01:23 +0545

    updated in part5.9

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const logger = require("./utils/logger");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const middleware = require("./utils/middleware");
 
 mongoose.set("strictQuery", false);
 
@@ -24,5 +26,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,30 +2,52 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/Blog");
 const logger = require("../utils/logger");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  console.log("auth header", authorization);
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
+
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
+  console.log("body", body);
+  console.log("req", request.params);
 
-  const user = await User.findById(body.userId);
+  try {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user.id,
-  });
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token invalid" });
+    }
 
-  const result = await blog.save();
-  logger.info("saved!");
-  user.blogs = user.blogs.concat(result._id);
-  await user.save();
-  response.status(201).json(result);
+    const user = await User.findById(decodedToken.id);
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user.id,
+    });
+
+    const result = await blog.save();
+    logger.info("saved!");
+    user.blogs = user.blogs.concat(result._id);
+    await user.save();
+    response.status(201).json(result);
+  } catch (error) {
+    response.status(401).json({ error: "token invalid" });
+  }
 });
 
 blogsRouter.delete("/:id", async (req, res) => {
@@ -40,16 +62,26 @@ blogsRouter.put("/:id", async (request, response) => {
   const id = request.params.id;
   const { likes } = request.body;
 
-  const updatedBlog = await Blog.findByIdAndUpdate(
-    id,
-    { likes },
-    { new: true }
-  );
+  console.log("Updating likes for blog with id:", id);
+  console.log("New likes:", likes);
 
-  if (!updatedBlog) {
-    return response.status(404).json({ error: "Blog not found" });
+  try {
+    // Find the blog by ID and update the likes
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id,
+      { likes },
+      { new: true }
+    );
+
+    if (!updatedBlog) {
+      return response.status(404).json({ error: "Blog not found" });
+    }
+
+    response.json(updatedBlog);
+  } catch (error) {
+    console.error("Error updating likes:", error);
+    response.status(500).json({ error: "Internal Server Error" });
   }
-  response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
